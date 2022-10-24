53a6f510bbb171e8717be5a343190b51b850ec36
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2022-10-24 14:51:28 -0300

    Organized gray shades

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./**/*.{html,js}", "node_modules/flowbite/**/*.js"],
   theme: {
+    colors: {
+      'g1' : '#f7f7f7',
+      'g3' : '#dedede',
+      'g5' : '#b2b2b2',
+      'g9' : '#404040',
+      'g10' : '#2e2e2e',
+      'g12' : '#0e0e0e',
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("flowbite/plugin")],
