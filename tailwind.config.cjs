3e1f391b0b37170eb61928790013c98ddca95537
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2022-10-25 22:09:30 -0300

    Gray shades

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,11 +9,16 @@ module.exports = {
   theme: {
     colors: {
       g1: "#f7f7f7",
+      g2: "#EDEDED",
       g3: "#dedede",
+      g4: "#CCCCCC",
       g5: "#b2b2b2",
+      g6: "#9C9C9C",
       g7: "#717171",
+      g8: "#595959",
       g9: "#404040",
       g10: "#2e2e2e",
+      g11: "#1A1A1A",
       g12: "#0e0e0e",
     },
     extend: {},
